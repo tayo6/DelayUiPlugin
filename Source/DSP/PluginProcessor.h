0ef9339f27db5656a4c8d19de1d053df91363334
Tayo <92905932+tayo6@users.noreply.github.com>
2026-07-09 16:28:20 +0100

    Update PluginProcessor.h

--- Source/DSP/PluginProcessor.h
+++ Source/DSP/PluginProcessor.h
@@ -19,10 +19,12 @@ public:
     bool isMidiEffect() const override { return false; }
     double getTailLengthSeconds() const override { return 0.0; }
     int getNumPrograms() override { return 1; }
+    
     int getCurrentProgram() override { return 0; }
-    void setCurrentProgram (int index) override {}
-    const juce::String getProgramName (int index) override { return {}; }
-    void changeProgramName (int index, const juce::String& newName) override {}
+    void setCurrentProgram (int /*index*/) override {}
+    const juce::String getProgramName (int /*index*/) override { return {}; }
+    void changeProgramName (int /*index*/, const juce::String& /*newName*/) override {}
+    
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
